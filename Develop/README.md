62a8752d1dddd1b7dcd6aeaceeabf90fcb483ac0
Benjamin Turnbull <benorule@outlook.com>
2020-07-18 22:02:12 +0930

    added formatting to README generation

--- Develop/README.md
+++ Develop/README.md
@@ -1,21 +1,22 @@
+# README Generator
 ## Table of Contents: 
  * [Description](#Description) 
 * [Installation Instructions](#Installation-Instructions) 
 * [Usage Information](#Usage-Information) 
-* [Contribution Guidlines](#Contribution-Guidlines) 
+* [Contribution Guidlines](#Contribution-Guidelines) 
 * [Test Instructions](#Test-Instructions) 
 * [Questions](#Questions) 
-# README generator
 ## Description 
-Generates a README file using Node.js
+Generates README file
 ## Installation Instructions 
-Download ZIP File or Clone Repository in GitHub
+Install by downloading zip file or cloning repo from GitHub
 ## Usage Information 
-Run application in terminal using node commands
+Use application in the terminal
 ## Contribution Guidelines 
-Submit a Pull Request via GitHub
+Contribute by creating a pull request on GitHub
 ## Test Instructions 
-Test application using Node command line input and README preview in VS Code
+Test the file using the terminal and README file preview on VS Code
 ## Questions 
  * GitHub: https://github.com/benorule
 * Email: benorule@outlook.com
+### License: MIT License
--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var readline = require('readline-sync');
+var inquirer = require('inquirer');
 
 // array of questions for user
 const questions = [
@@ -10,7 +11,8 @@ const questions = [
     "Enter the contribution guidlines for the application: ",
     "Enter the test instructions for the application: ",
     "Enter your GitHub username: ",
-    "Enter your email address: "
+    "Enter your email address: ",
+    "Enter the license for the application: "
 ];
 
 const answers = [
@@ -20,28 +22,29 @@ for (i = 0; i < questions.length; i++) {
     answers.push(readline.question(questions[i]));
 }
 
-var stream = fs.createWriteStream("README.md");
-stream.once('open', function (fd) {
-    stream.write("## Table of Contents: \n * [Description](#Description) \n");
-    stream.write("* [Installation Instructions](#Installation-Instructions) \n");
-    stream.write("* [Usage Information](#Usage-Information) \n");
-    stream.write("* [Contribution Guidlines](#Contribution-Guidelines) \n");
-    stream.write("* [Test Instructions](#Test-Instructions) \n");
-    stream.write("* [Questions](#Questions) \n");
-    stream.write("# " + answers[0] + "\n");
-    stream.write("## Description \n" + answers[1] + "\n");
-    stream.write("## Installation Instructions \n" + answers[2] + "\n");
-    stream.write("## Usage Information \n" + answers[3] + "\n");
-    stream.write("## Contribution Guidelines \n" + answers[4] + "\n");
-    stream.write("## Test Instructions \n" + answers[5] + "\n");
-    stream.write("## Questions \n * GitHub: https://github.com/" + answers[6] + "\n");
-    stream.write("* Email: " + answers[7] + "\n");
-    stream.end();
-});
+    var stream = fs.createWriteStream("README.md");
+    stream.once('open', function (fd) {
+        stream.write("# " + answers[0] + "\n");
+        stream.write("## Table of Contents: \n * [Description](#Description) \n");
+        stream.write("* [Installation Instructions](#Installation-Instructions) \n");
+        stream.write("* [Usage Information](#Usage-Information) \n");
+        stream.write("* [Contribution Guidlines](#Contribution-Guidelines) \n");
+        stream.write("* [Test Instructions](#Test-Instructions) \n");
+        stream.write("* [Questions](#Questions) \n");
+        stream.write("## Description \n" + answers[1] + "\n");
+        stream.write("## Installation Instructions \n" + answers[2] + "\n");
+        stream.write("## Usage Information \n" + answers[3] + "\n");
+        stream.write("## Contribution Guidelines \n" + answers[4] + "\n");
+        stream.write("## Test Instructions \n" + answers[5] + "\n");
+        stream.write("## Questions \n * GitHub: https://github.com/" + answers[6] + "\n");
+        stream.write("* Email: " + answers[7] + "\n");
+        stream.write("* License: " + answers[8] + "\n");
+        stream.end();
+    });
 
-// // function to initialize program
-function init() {
-}
+    // // function to initialize program
+    function init() {
+    }
 
-// // function call to initialize program
-init();
+    // // function call to initialize program
+    init();
