f8e38a94e12fd231a3f99a774481c81e02277809
Benjamin Turnbull <benorule@outlook.com>
2020-07-18 15:17:23 +0930

    programmed basic functionality

---
+++ Develop/README.md
@@ -0,0 +1,11 @@
+# README generator
+## Description 
+generates readme file
+## Installation Instructions 
+install with the method of...
+## Usage Information 
+use the application in terminal
+## Contribution Guidelines 
+contribute to the application on github
+## Test Instructions 
+test application in terminal using node index.js command
--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,37 @@
+var fs = require('fs');
+var readline = require('readline-sync');
+
 // array of questions for user
 const questions = [
-
+    "Enter the title of the application: ",
+    "Enter the description of the application: ",
+    "Enter the installation instructions for the application: ",
+    "Enter the usage information for the application: ",
+    "Enter the contribution guidlines for the application: ",
+    "Enter the test instructions for the application: ",
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
+const answers = [
+];
+
+for (i = 0; i < questions.length; i++) {
+    answers.push(readline.question(questions[i]));
 }
 
-// function to initialize program
+var stream = fs.createWriteStream("README.md");
+stream.once('open', function (fd) {
+    stream.write("# " + answers[0] + "\n");
+    stream.write("## Description \n" + answers[1] + "\n");
+    stream.write("## Installation Instructions \n" + answers[2] + "\n");
+    stream.write("## Usage Information \n" + answers[3] + "\n");
+    stream.write("## Contribution Guidelines \n" + answers[4] + "\n");
+    stream.write("## Test Instructions \n" + answers[5] + "\n");
+    stream.end();
+});
+
+// // function to initialize program
 function init() {
-
 }
 
-// function call to initialize program
+// // function call to initialize program
 init();
