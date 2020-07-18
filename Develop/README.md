7b275580a21a5e4decfaaace1175678f454b07dc
Benjamin Turnbull <benorule@outlook.com>
2020-07-18 17:37:53 +0930

    added table of contents

--- Develop/README.md
+++ Develop/README.md
@@ -1,11 +1,21 @@
+## Table of Contents: 
+ * [Description](#Description) 
+* [Installation Instructions](#Installation-Instructions) 
+* [Usage Information](#Usage-Information) 
+* [Contribution Guidlines](#Contribution-Guidlines) 
+* [Test Instructions](#Test-Instructions) 
+* [Questions](#Questions) 
 # README generator
 ## Description 
-generates readme file
+Generates a README file using Node.js
 ## Installation Instructions 
-install with the method of...
+Download ZIP File or Clone Repository in GitHub
 ## Usage Information 
-use the application in terminal
+Run application in terminal using node commands
 ## Contribution Guidelines 
-contribute to the application on github
+Submit a Pull Request via GitHub
 ## Test Instructions 
-test application in terminal using node index.js command
+Test application using Node command line input and README preview in VS Code
+## Questions 
+ * GitHub: https://github.com/benorule
+* Email: benorule@outlook.com
--- Develop/index.js
+++ Develop/index.js
@@ -9,6 +9,8 @@ const questions = [
     "Enter the usage information for the application: ",
     "Enter the contribution guidlines for the application: ",
     "Enter the test instructions for the application: ",
+    "Enter your GitHub username: ",
+    "Enter your email address: "
 ];
 
 const answers = [
@@ -20,12 +22,20 @@ for (i = 0; i < questions.length; i++) {
 
 var stream = fs.createWriteStream("README.md");
 stream.once('open', function (fd) {
+    stream.write("## Table of Contents: \n * [Description](#Description) \n");
+    stream.write("* [Installation Instructions](#Installation-Instructions) \n");
+    stream.write("* [Usage Information](#Usage-Information) \n");
+    stream.write("* [Contribution Guidlines](#Contribution-Guidelines) \n");
+    stream.write("* [Test Instructions](#Test-Instructions) \n");
+    stream.write("* [Questions](#Questions) \n");
     stream.write("# " + answers[0] + "\n");
     stream.write("## Description \n" + answers[1] + "\n");
     stream.write("## Installation Instructions \n" + answers[2] + "\n");
     stream.write("## Usage Information \n" + answers[3] + "\n");
     stream.write("## Contribution Guidelines \n" + answers[4] + "\n");
     stream.write("## Test Instructions \n" + answers[5] + "\n");
+    stream.write("## Questions \n * GitHub: https://github.com/" + answers[6] + "\n");
+    stream.write("* Email: " + answers[7] + "\n");
     stream.end();
 });
 
